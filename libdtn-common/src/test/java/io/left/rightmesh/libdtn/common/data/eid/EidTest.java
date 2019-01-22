221449922d03579a1aa15210daf24a9f32ed1707
Penjani Chavula <penjani@rightmesh.io>
2019-01-22 14:44:43 -0800

    Wrote some tests for the BaseClaEid class and some parts of the BaseEidFactory

--- libdtn-common/src/test/java/io/left/rightmesh/libdtn/common/data/eid/EidTest.java
+++ libdtn-common/src/test/java/io/left/rightmesh/libdtn/common/data/eid/EidTest.java
@@ -1,8 +1,4 @@
-package io.left.rightmesh.libdtn.common.data;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
+package io.left.rightmesh.libdtn.common.data.eid;
 
 import io.left.rightmesh.libdtn.common.data.eid.BaseEidFactory;
 import io.left.rightmesh.libdtn.common.data.eid.DtnEid;
@@ -12,6 +8,8 @@ import io.left.rightmesh.libdtn.common.data.eid.EidFormatException;
 import io.left.rightmesh.libdtn.common.data.eid.EidIpn;
 
 import org.junit.Test;
+
+import static org.junit.Assert.*;
 
 /**
  * Test class for Endpoint IDs.
@@ -58,5 +56,64 @@ public class EidTest {
         } catch (EidFormatException efe) {
             fail();
         }
+    }
+
+    @Test
+    public void testClaEid() {
+        System.out.println("[+] eid: testing ClaEid Scheme");
+        String eidString = "cla:stcp:1.2.3.4:4557";
+        try {
+            Eid cla = eidFactory.create(eidString);
+            assertEquals(eidString, cla.getEidString());
+            assertEquals("cla", cla.getScheme());
+
+            assertTrue(cla.matches(cla));
+            assertFalse(cla.matches(null));
+
+            Eid cla2 = cla.copy();
+            assertTrue(cla.matches(cla2));
+            assertEquals(ClaEid.EID_CLA_IANA_VALUE, cla2.ianaNumber());
+
+            cla = ((BaseClaEid) cla).setPath("/test");
+            assertEquals("/test", ((BaseClaEid) cla).getPath());
+            assertEquals("", (((BaseClaEid) cla2).getPath()));
+
+            cla2 = ((BaseClaEid) cla).setPath("test2");
+            assertEquals("/test2", ((BaseClaEid) cla2).getPath());
+
+            assertEquals("stcp", (((BaseClaEid) cla2).getClaName()));
+            assertEquals("1.2.3.4:4557", (((BaseClaEid) cla2).getClaSpecificPart()));
+
+
+            Eid cla3 = eidFactory.create(eidString + "0");
+            assertFalse(cla.matches(cla3));
+
+            Eid cla4 = eidFactory.create("dtn:marsOrbital");
+            assertFalse(cla.matches(cla4));
+
+        } catch (EidFormatException efe) {
+            fail();
+        }
+
+        BaseEidFactory exceptionThrowerFactory = new BaseEidFactory(true);
+        try {
+            Eid cla = exceptionThrowerFactory.create(eidString);
+        } catch (ClaEidParser.UnknownClaName e) {
+            assertTrue(e.getMessage().contains("claName unknown"));
+        } catch (EidFormatException efe) {
+            fail();
+        }
+
+
+    }
+
+    @Test
+    public void testApiEid() {
+
+    }
+
+    @Test
+    public void testBaseEidFactory() {
+
     }
 }
